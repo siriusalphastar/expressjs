474382f167b2d8ee9fde1d1a08065669ca1ed611
siriusalphastar <ihlosiy@gmail.com>
2023-05-05 19:06:09 -0400

    express server setup

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const app = express(); // app allows us to set up our entire server
+const port = 3000;
 
+// app.listen(3000)  // simplified version
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+}); // make our server run
