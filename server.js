99161e5be507b60730c30a875bde6b28b0453f77
siriusalphastar <ihlosiy@gmail.com>
2023-05-05 19:26:02 -0400

    basic routing - created route at root path / & sent data back to user

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express(); // app allows us to set up our entire server
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("hola, user");
+  console.log("sending data back down to a user");
+});
+
 // app.listen(3000)  // simplified version
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
