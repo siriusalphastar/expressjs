eed1454b8b26d6121a9ebf3acdbb21bc8bff38b0
siriusalphastar <ihlosiy@gmail.com>
2023-05-06 01:20:36 -0400

    rendering HTML

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express(); // app allows us to set up our entire server
 const port = 3000;
 
+app.set("view engine", "ejs"); // setting ejs as a view engine
+
 app.get("/", (req, res) => {
+  res.render("index"); // rendering index.ejs HTML file
+
   // res.send("hola, user"); // sending data to a user
   // res.sendStatus(500); // sending status code to a user
   // res.status(500).send("hi"); // sending status code w/ a message to a user
   // res.status(500).json({message: "Internal server error."}); // sending status code w/ JSON
   // res.json({ message: "Default success status 200" }); // default status code w/ JSON
-  res.download("server.js"); // sending a file to download to a user
+  // res.download("server.js"); // sending a file to download to a user
 });
 
 // app.listen(3000)  // simplified version
